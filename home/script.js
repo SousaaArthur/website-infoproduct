40b5480916b9715ccb9d252f54774af203f12ff4
SousaaArthur <arthursousaa2004@gmail.com>
2023-06-15 03:07:36 -0300

    Ataulização 1.2

--- home/script.js
+++ home/script.js
@@ -5,13 +5,13 @@ let body = document.documentElement;
 function toView(){
     hide.classList.remove('hide');
     document.body.style.overflow = 'hidden'
-
-
+    document.body.style.overflowX = 'hidden'
 }
 
 function closePreview(){
     hide.classList.add('hide')
     document.body.style.overflow = 'visible'
+    document.body.style.overflowX = 'hidde'
 }
 
 //Code Scrolled Navbar
@@ -23,3 +23,28 @@ window.addEventListener('scroll', function(){
         navbar.classList.remove('scrolled');
     }
 })
+
+//Code slider assessment
+// Função para mover o carrossel para a esquerda
+function moveCarouselLeft() {
+    const carousel = document.querySelector('.card-group-assessment');
+    carousel.scrollBy({
+        left: -250, // Quantidade de pixels para mover o carrossel
+        behavior: 'smooth' // Adiciona uma animação suave
+    });
+}
+
+// Função para mover o carrossel para a direita
+function moveCarouselRight() {
+    const carousel = document.querySelector('.card-group-assessment');
+    carousel.scrollBy({
+        left: 250, // Quantidade de pixels para mover o carrossel
+        behavior: 'smooth' // Adiciona uma animação suave
+    });
+}
+
+// Adiciona os ouvintes de evento aos botões
+const btnLeft = document.querySelector('.btn-slider.btn-left');
+const btnRight = document.querySelector('.btn-slider.btn-right');
+btnLeft.addEventListener('click', moveCarouselLeft);
+btnRight.addEventListener('click', moveCarouselRight);
